afdb1bdcf79f2858859d92365a6b8187a655e686
klarich <22804730+klarich@users.noreply.github.com>
2023-08-11 14:51:36 -0600

    Start incorporating reagents

--- ts_utils.py
+++ ts_utils.py
@@ -1,6 +1,20 @@
+from typing import List
+
 import pandas as pd
 from rdkit import Chem
 
+from reagent import Reagent
+
+
+def create_reagents(filename: str) -> List[Reagent]:
+    # TODO: add option to only select the first n reagents
+    reagent_list = []
+    with open(filename, 'r') as f:
+        for line in f.readlines():
+            smiles, reagent_name = line.split()
+            reagent = Reagent(reagent_name=reagent_name, smiles=smiles)
+            reagent_list.append(reagent)
+    return reagent_list
 
 def read_reagents(reagent_file_list, num_to_select):
     """
@@ -9,10 +23,10 @@ def read_reagents(reagent_file_list, num_to_select):
     :param num_to_select: select how many reagents to read, mostly a development function
     """
     reagent_df_list = []
-    for r in reagent_file_list:
-        df = pd.read_csv(r, sep=" ", names=["SMILES", "Name"])
-        df = df.head(num_to_select).copy()
-        # add an RDKit molecule to the dataframe
-        df['mol'] = df.SMILES.apply(Chem.MolFromSmiles)
-        reagent_df_list.append(df)
+    for idx, r in reagent_file_list:
+        # df = pd.read_csv(r, sep=" ", names=["SMILES", "Name"])
+        # df = df.head(num_to_select).copy()
+        # # add an RDKit molecule to the dataframe
+        # df['mol'] = df.SMILES.apply(Chem.MolFromSmiles)
+        # reagent_df_list.append(df)
     return reagent_df_list
